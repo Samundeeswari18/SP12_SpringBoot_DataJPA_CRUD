c76f1015bd0d11441cdf9e94aba0fd15675e9fa2
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-06 20:35:01 -0400

    SpringBoot dataJPA CRUD Database

---
+++ src/main/java/com/sam/spring_boot_data_jpa_sp12/SpringBootDataJpaSp12Application.java
@@ -0,0 +1,13 @@
+package com.sam.spring_boot_data_jpa_sp12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaSp12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDataJpaSp12Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/spring_boot_data_jpa_sp12/SpringBootDataJpaSp12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.spring_boot_data_jpa_sp12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaSp12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
